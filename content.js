a0619764bae94bc717eff63bb93fdf18d362d09e
Ricky Hu <ricky9667@gmail.com>
2025-05-27 22:57:10 +0800

    feat: 🎸 replace traditional and simplified mandarin text

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 function replaceTextInNode(node) {
   if (node.nodeType === Node.TEXT_NODE) {
     node.textContent = node.textContent.replace(/Chinese Taipei/gi, 'Taiwan');
+    node.textContent = node.textContent.replace(/中華台北/gi, '台灣');
+    node.textContent = node.textContent.replace(/中华台北/gi, '台湾');
   } else {
     node.childNodes.forEach(child => replaceTextInNode(child));
   }
